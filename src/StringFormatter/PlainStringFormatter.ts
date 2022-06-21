1069920cd550af0552ea2a28d512b4ff90f6476e
Garrett Charles <garrettguycharles@gmail.com>
2022-06-21 10:40:22 -0600

    Stripping away non-boilerplate code

--- src/StringFormatter/PlainStringFormatter.ts
+++
@@ -1,9 +0,0 @@
-import {IStringFormatter} from "./abstract/StringFormatter";
-
-export class PlainStringFormatter implements IStringFormatter {
-    format(s: string): HTMLElement[] {
-        const toReturn = document.createElement('span');
-        toReturn.appendChild(document.createTextNode(s));
-        return [toReturn];
-    }
-}
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,8 @@
-import {random_range} from "./utils/utils";
 import {Tickable} from "./utils/Tickable";
-import {IStringFormatter} from "./StringFormatter/abstract/StringFormatter";
-import {SpookyStringFormatter} from "./StringFormatter/SpookyStringFormatter";
-import {PlainStringFormatter} from "./StringFormatter/PlainStringFormatter";
 
-export class App extends Tickable {
-    input = document.getElementById('input') as HTMLInputElement;
-    output = document.getElementById('output');
-    stringFormatter: IStringFormatter = new PlainStringFormatter();
-
-    spookyButton = document.getElementById('spookyButton');
-    plainButton = document.getElementById('plainButton');
-    backwardsButton = document.getElementById('backwardsButton');
-
-    init(): void {
-        this.spookyButton.addEventListener('click', () => {
-            this.stringFormatter = new SpookyStringFormatter();
-        });
-
-        this.plainButton.addEventListener('click', () => {
-            this.stringFormatter = new PlainStringFormatter();
-        });
-
-        // don't forget to plug in the BackwardsFormatter here!
-        this.backwardsButton.addEventListener('click', () => {
-            console.error(`Backwards button is not implemented!!`);
-        });
-    }
-
+export class App {
     // singleton pattern
     private constructor() {
-        super();
-        this.setTickDelay(1000/30)
-        this.init();
     }
 
     private static instance: App;
@@ -43,25 +13,6 @@ export class App extends Tickable {
         }
 
         return App.instance;
-    }
-
-    onTick(): Promise<void> {
-        // remove all children of output div
-        while (this.output.children.length) {
-            this.output.children[0].remove();
-        }
-
-        const stringPresentationElements = this.stringFormatter.format(this.input.value);
-
-        for (const el of stringPresentationElements) {
-            this.output.appendChild(el);
-        }
-
-        return Promise.resolve();
-    }
-
-    protected shouldContinueTicking(): boolean {
-        return true;
     }
 }
 
